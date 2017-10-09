f44f3c88cd53853684ba40494c93d57f0680ddd1
Kevin-Brosnan <kevin.c.brosnan@gmail.com>
2017-10-09 15:27:18 +0100

    energy_fidelity - code + documentation + tests

---
+++ R/energy_fidelity.R
@@ -0,0 +1,37 @@
+#' Calculate the energy function for fidelity to underlying data
+#'
+#' The \code{energy_fidelity} function calculates the energy associated with
+#' the difference between the underlying observed data and the current
+#' configuration of the model.
+#'
+#' @param y the underlying observed lattice grid
+#' @param f the current configuration of the model for which to compare
+#'
+#' @return A measure of the discordance between the observed data and the model
+#'
+#' @author Kevin Brosnan (\email{kevin.c.brosnan@@gmail.com})
+#'
+#' @examples
+#'   y <- matrix(sample(c(-1, 1), size = 16, prob = c(0.6, 0.4)), nrow = 4)
+#'   f <- matrix(sample(c(-1, 1), size = 16, prob = c(0.65, 0.35), nrow = 4))
+#'   energy_fidelity()
+#'
+#' @export
+
+energy_fidelity <- function(y, f) {
+
+  if (class(y) != "gatingfc_grid" || !is.matrix(y)) {
+    stop("\"y\" and \"f\" must be a matrix, data frame or of class \"gatingfc_grid\"")
+  }
+
+  if (!is.matrix(f) || class(f) != "gatingfc_grid") {
+    stop("\"y\" and \"f\" must be a matrix, data frame or of class \"gatingfc_grid\"")
+  }
+
+  if (nrow(y) != nrow(f) || ncol(y) != ncol(f)) {
+    stop("\"y\" and \"f\" are not of the same size")
+  }
+
+  energy <- -sum(y * f)
+  return(energy)
+}
---
+++ tests/testthat/test-energy_fidelity.R
@@ -0,0 +1,42 @@
+context("Calculating Fidelity to Data Energy")
+
+test_that("If y and f not matrix or of class gatingfc_grid", {
+  y <- matrix(sample(c(-1, 1), size = 16), nrow = 4)
+  f <- y
+  class(f) <- "gatingfc"
+  expect_error(energy_fidelity(y = y, f = f),
+               "\"y\" and \"f\" must be a matrix, data frame or of class \"gatingfc_grid\"")
+  class(y) <- "gatingfc_grid"
+  expect_error(energy_fidelity(y = y, f = f),
+               "\"y\" and \"f\" must be a matrix, data frame or of class \"gatingfc_grid\"")
+  f <- matrix(sample(c(-1, 1), size = 16), nrow = 4)
+  y <- f
+  class(y) <- "silly_class"
+  expect_error(energy_fidelity(y = y, f = f),
+               "\"y\" and \"f\" must be a matrix, data frame or of class \"gatingfc_grid\"")
+  class(f) <- "gatingfc_grid"
+  expect_error(energy_fidelity(y = y, f = f),
+               "\"y\" and \"f\" must be a matrix, data frame or of class \"gatingfc_grid\"")
+
+})
+
+test_that("If f and y not of the same dimension", {
+  f <- matrix(sample(c(-1, 1), size = 16), nrow = 4)
+  y <- f[-1, ]
+
+  expect_error(energy_fidelity(y = y, f = f),
+               "\"y\" and \"f\" are not of the same size")
+})
+
+test_that("Check that fidelity energy works correctly", {
+  y <- matrix(c(-1, 1, -1, -1), nrow = 2)
+  f1 <- x * -1
+  f2 <- f1
+  f2[1] <- f2 * -1
+
+  expect_equal(energy_fidelity(y = y, f = f1), +2)
+  expect_equal(energy_fidelity(y = y, f = f2), +2)
+  class(y) <- "gatingfc_grid"
+  expect_equal(energy_fidelity(y = y, f = f1), +2)
+  expect_equal(energy_fidelity(y = y, f = f2), +2)
+})
