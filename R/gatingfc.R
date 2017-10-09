7fff9bbc6435fcc5b107b41fd21960cf60b1dc5b
Kevin-Brosnan <kevin.c.brosnan@gmail.com>
2017-10-09 13:39:26 +0100

    Package Documentation

---
+++ R/gatingfc.R
@@ -0,0 +1,17 @@
+#' gatingfc: automated gating of cytometry data
+#'
+#' gatingfc provides a framework for the statsitical analysis, and particularly
+#' the gating, of flow cytometry data.
+#'
+#' It addresses four of the objectives of the flow cytometry analysis framework:
+#'
+#' \itemize {
+#' \item Normalisation
+#' \item Outlier Removal
+#' \item Gating
+#' \item Cluster Labelling
+#' }
+#'
+#' To learn how to use gatingfc, please see the introductory vignettes:
+#' `browseVignettes(package = "gatingfc")`
+"_PACKAGE"
