8157232959cfa02a4d5bcbbe78ff5de42ff6de28
Kevin-Brosnan <kevin.c.brosnan@gmail.com>
2017-10-09 15:27:47 +0100

    gating - code + documentation

---
+++ R/gating.R
@@ -0,0 +1,60 @@
+#' gating
+#'
+#' @export gating
+gating <- function(x, var.names = NULL) {
+
+####---- 1. Verify Inputs --------------------------------------------------####
+
+  # If x is not a data.frame then stop
+  if (!is.data.frame(x)) {
+    stop('Input object must be a data frame of dimension N times p')
+  }
+
+  # Make sure x is of the correct dimension and set up var.names accordingly
+  if (ncol(x) > 2 && is.null(var.names)) {
+    warning(paste('Input object has greater than 2 columns of data and',
+                  'var.names is not specified, using the first 2 columns',
+                  'of input object. Please specify var.names for other',
+                  'pairs of variables.', sep = "\n  "))
+    var.names <- c(1, 2)
+  } else if (ncol(x) < 2) {
+    stop('Input object must be a data frame with at least 2 columns.')
+  } else if (ncol(x) == 2) {
+    var.names <- c(1, 2)
+  }
+
+####---- 2. Construct Lattice Grid -----------------------------------------####
+
+  # Retain the rows of interest in x
+  x <- x[, var.names]
+  # Create the lattice grid
+  grid <- make_grid(x)
+  # Get the dimension of the lattice grid
+  grid.dim <- nrow(grid)
+
+####---- 3. Outlier Detection & Removal ------------------------------------####
+
+  # Bottom Edge and Right Edge Observations set to zero
+  grid[, grid.dim] <- grid[grid.dim, ] <- 0
+  # Record the positions of edge values in original x
+  extreme.values <- unique(c(which(x[,1] == grid.dim - 1),
+                             which(x[,2] == grid.dim - 1)))
+  # Create removals vector which returns 1 for outlier, 0 otherwise
+  removals <- rep(0, times = nrow(x))
+  removals[extreme.values] <- 1
+
+####---- 4. Gating ---------------------------------------------------------####
+
+
+
+####---- 5. Cluster Labelling ----------------------------------------------####
+
+
+####---- 6. Reconstruct Data Frame -----------------------------------------####
+
+
+####---- 7. Construct Output -----------------------------------------------####
+
+  output <- structure(list())
+  return(output)
+}
